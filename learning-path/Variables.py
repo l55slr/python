7a1d877941f93b94c180945a4f10e2118abeea17
l55slr <salaradel12@icloud.com>
2026-05-30 15:55:55 +0300

    overwriting case

--- learning-path/Variables.py
+++ learning-path/Variables.py
@@ -3,3 +3,11 @@
 player_health = 1000
 print(player_health)
 
+
+# the variable overwriting case:
+acceleration = 10
+acceleration = 20
+print(acceleration)
+# The line acceleration = 20 reassigns the value of acceleration to 20.
+# It overwrites whatever was being held in the acceleration variable before (10 in this case).
+
