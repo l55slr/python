e3864d4b02d293193fedd760195216d65c609921
l55slr <salaradel12@icloud.com>
2026-05-30 16:05:07 +0300

    Math operations

--- learning-path/Variables.py
+++ learning-path/Variables.py
@@ -11,3 +11,11 @@ print(acceleration)
 # The line acceleration = 20 reassigns the value of acceleration to 20.
 # It overwrites whatever was being held in the acceleration variable before (10 in this case).
 
+
+# Math CH, We can also perform math operations on variables. you most follow the order of calculation (PEMDAS) when doing so.
+player_health = 1000
+armor_multiplier = 2
+armored_health = player_health * armor_multiplier
+print(armored_health)
+
+
