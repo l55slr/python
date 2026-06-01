1cdf9cadd991134ed7cafa9c823d87ba9c16f005
l55slr <salaradel12@icloud.com>
2026-06-01 21:16:35 +0300

    Parameters vs. Arguments

--- learning-path/functions.py
+++ learning-path/functions.py
@@ -8,3 +8,17 @@ Ends the current function's execution
 Provides a value (or values) back to the caller of the function
 Does not print anything to the console (unless the return value is later print()ed)
 """
+
+"""
+Parameters vs. Arguments
+Parameters:
+are the names used for inputs when defining a function
+Arguments:
+are the values of the inputs supplied when a function is called.
+"""
+# a and b are parameters
+def add(a, b):
+    return a + b
+
+# 5 and 6 are arguments
+sum = add(5, 6)
