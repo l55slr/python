ee860e950bb240ad1f369491ae5297d1e7092cd9
l55slr <salaradel12@icloud.com>
2026-06-03 17:59:41 +0300

    Making a function with multiple  retun value

--- learning-path/functions.py
+++ learning-path/functions.py
@@ -34,3 +34,29 @@ def add(a, b):
 
 # 5 and 6 are arguments
 sum = add(5, 6)
+
+
+#Enchant and Attack
+#The function part is my own coding line 41-45
+def enchant_and_attack(target_health, damage, weapon):
+    enchanted_damage = damage +10
+    new_health = target_health - enchanted_damage
+    enchanted_weapon = "enchanted" + weapon
+    return enchanted_weapon, new_health
+
+def test(target_health, damage, weapon):
+    print(f"The target has {target_health} health.")
+    print(f"{weapon} base damage: {damage}... Enchanting and attacking.")
+    enchanted_weapon, new_health = enchant_and_attack(target_health, damage, weapon)
+    print(f"The target has been attacked with the {enchanted_weapon}.")
+    print(f"The target has {new_health} health remaining.")
+    print("=====================================")
+
+
+def main():
+    test(100, 50, "sword")
+    test(500, 100, "axe")
+    test(1000, 250, "bow")
+
+
+main()
