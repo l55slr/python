e68ddc41c98eb52475abeb01f8d1a617bf6de4e1
l55slr <salaradel12@icloud.com>
2026-06-04 14:53:01 +0300

    Mathematics signs

---
+++ learning-path/computing.py
@@ -0,0 +1,18 @@
+#Mathematics in python
+#Addition
+2 + 1 # 3
+
+#Subtraction
+2 - 1 # 1
+
+#Multiplication
+2 * 2 # 4
+
+#Division
+3 / 2 #This will give us a float result, 1.5
+
+#Floor Division
+11//2 #will give us 5 not 5.5, it rounds down to the nearest whole number no matter what even if the result is 5.9 it will still give us 5
+
+#Exponents
+2 ** 3 #This will give us 8, which is 2 multiplied by itself 3 times
