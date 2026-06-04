7860f1029ef69083f2aa4eb781de627912da27d3
l55slr <salaradel12@icloud.com>
2026-06-04 18:25:44 +0300

    elif Statement

--- learning-path/Comparison.py
+++ learning-path/Comparison.py
@@ -11,8 +11,20 @@
 if 2 > 1:
     print("2 is greater than 1")
 
+
 #If-Else Statements
 if 2 < 1:
     print("2 is less than 1")
 else:
     print("2 is not less than 1")
+
+
+#elif Statements
+if score > high_score:
+    print("High score beat!")
+elif score > second_highest_score:
+    print("You got second place!")
+elif score > third_highest_score:
+    print("You got third place!")
+else:
+    print("Better luck next time")
