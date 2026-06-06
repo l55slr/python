4a865e0e8d6ab32214cc53f1eba2a565e285b151
l55slr <salaradel12@icloud.com>
2026-06-06 18:37:18 +0300

    loops with start&end

--- learning-path/loop.py
+++ learning-path/loop.py
@@ -4,3 +4,7 @@ for i in range(0, 10):
 #this will print the numbers from 0-9 because the range function generates numbers from the start value (0) up to but not including the stop value (10)
 
 
+#here the loop got starting value 5 and the stop value is given by the parameter "end"
+def print_numbers_from_five_to(end):
+    for f in range(5, end):
+        print(f)
