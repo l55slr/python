169af9c2336ad105bc4f08d4fa6ca1b95b2d710e
l55slr <salaradel12@icloud.com>
2026-06-06 18:34:29 +0300

    for loop

---
+++ learning-path/loop.py
@@ -0,0 +1,6 @@
+#A "for loop" in Python is written like this:
+for i in range(0, 10):
+    print(i)
+#this will print the numbers from 0-9 because the range function generates numbers from the start value (0) up to but not including the stop value (10)
+
+
